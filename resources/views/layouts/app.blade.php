7d5fe4eec500118b9cee0b023dbabedb29f79f05
Elmer Huitz <elmohuit@gmail.com>
2020-11-09 21:35:30 -0600

    reconfigure welcome.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                <div><img src="/img/logo.jpeg" alt="Twinling Smiles Logo" style= "height:50px" class= pr-3></div>
+                <div><img src="/img/logo.png" alt="Twinling Smiles Logo" style= "height:50px" class= pr-3></div>
                   <!--  {{ config('app.name', 'Laravel') }}  -->
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
